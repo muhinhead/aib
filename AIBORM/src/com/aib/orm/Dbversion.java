7c1e6edc40688f927efaf6f92532029aecc29c26
mukhin.nick <mukhin.nick@b3c5fbc4-fa22-48f7-9f9e-e458a875d5c6>
2013-05-06 18:25:49 +0000

    the server complete

--- AIBORM/src/com/aib/orm/Dbversion.java
+++ AIBORM/src/com/aib/orm/Dbversion.java
@@ -8,38 +8,41 @@ import com.aib.orm.dbobject.Triggers;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class Product extends DbObject  {
+public class Dbversion extends DbObject  {
     private static Triggers activeTriggers = null;
-    private Integer productId = null;
-    private String descr = null;
+    private Integer dbversionId = null;
+    private Integer versionId = null;
+    private String version = null;
 
-    public Product(Connection connection) {
-        super(connection, "product", "product_id");
-        setColumnNames(new String[]{"product_id", "descr"});
+    public Dbversion(Connection connection) {
+        super(connection, "dbversion", "dbversion_id");
+        setColumnNames(new String[]{"dbversion_id", "version_id", "version"});
     }
 
-    public Product(Connection connection, Integer productId, String descr) {
-        super(connection, "product", "product_id");
-        setNew(productId.intValue() <= 0);
-//        if (productId.intValue() != 0) {
-            this.productId = productId;
+    public Dbversion(Connection connection, Integer dbversionId, Integer versionId, String version) {
+        super(connection, "dbversion", "dbversion_id");
+        setNew(dbversionId.intValue() <= 0);
+//        if (dbversionId.intValue() != 0) {
+            this.dbversionId = dbversionId;
 //        }
-        this.descr = descr;
+        this.versionId = versionId;
+        this.version = version;
     }
 
     public DbObject loadOnId(int id) throws SQLException, ForeignKeyViolationException {
-        Product product = null;
+        Dbversion dbversion = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String stmt = "SELECT product_id,descr FROM product WHERE product_id=" + id;
+        String stmt = "SELECT dbversion_id,version_id,version FROM dbversion WHERE dbversion_id=" + id;
         try {
             ps = getConnection().prepareStatement(stmt);
             rs = ps.executeQuery();
             if (rs.next()) {
-                product = new Product(getConnection());
-                product.setProductId(new Integer(rs.getInt(1)));
-                product.setDescr(rs.getString(2));
-                product.setNew(false);
+                dbversion = new Dbversion(getConnection());
+                dbversion.setDbversionId(new Integer(rs.getInt(1)));
+                dbversion.setVersionId(new Integer(rs.getInt(2)));
+                dbversion.setVersion(rs.getString(3));
+                dbversion.setNew(false);
             }
         } finally {
             try {
@@ -48,7 +51,7 @@ public class Product extends DbObject  {
                 if (ps != null) ps.close();
             }
         }
-        return product;
+        return dbversion;
     }
 
     protected void insert() throws SQLException, ForeignKeyViolationException {
@@ -57,26 +60,27 @@ public class Product extends DbObject  {
          }
          PreparedStatement ps = null;
          String stmt =
-                "INSERT INTO product ("+(getProductId().intValue()!=0?"product_id,":"")+"descr) values("+(getProductId().intValue()!=0?"?,":"")+"?)";
+                "INSERT INTO dbversion ("+(getDbversionId().intValue()!=0?"dbversion_id,":"")+"version_id,version) values("+(getDbversionId().intValue()!=0?"?,":"")+"?,?)";
          try {
              ps = getConnection().prepareStatement(stmt);
              int n = 0;
-             if (getProductId().intValue()!=0) {
-                 ps.setObject(++n, getProductId());
+             if (getDbversionId().intValue()!=0) {
+                 ps.setObject(++n, getDbversionId());
              }
-             ps.setObject(++n, getDescr());
+             ps.setObject(++n, getVersionId());
+             ps.setObject(++n, getVersion());
              ps.execute();
          } finally {
              if (ps != null) ps.close();
          }
          ResultSet rs = null;
-         if (getProductId().intValue()==0) {
-             stmt = "SELECT max(product_id) FROM product";
+         if (getDbversionId().intValue()==0) {
+             stmt = "SELECT max(dbversion_id) FROM dbversion";
              try {
                  ps = getConnection().prepareStatement(stmt);
                  rs = ps.executeQuery();
                  if (rs.next()) {
-                     setProductId(new Integer(rs.getInt(1)));
+                     setDbversionId(new Integer(rs.getInt(1)));
                  }
              } finally {
                  try {
@@ -102,12 +106,13 @@ public class Product extends DbObject  {
             }
             PreparedStatement ps = null;
             String stmt =
-                    "UPDATE product " +
-                    "SET descr = ?" + 
-                    " WHERE product_id = " + getProductId();
+                    "UPDATE dbversion " +
+                    "SET version_id = ?, version = ?" + 
+                    " WHERE dbversion_id = " + getDbversionId();
             try {
                 ps = getConnection().prepareStatement(stmt);
-                ps.setObject(1, getDescr());
+                ps.setObject(1, getVersionId());
+                ps.setObject(2, getVersion());
                 ps.execute();
             } finally {
                 if (ps != null) ps.close();
@@ -120,48 +125,31 @@ public class Product extends DbObject  {
     }
 
     public void delete() throws SQLException, ForeignKeyViolationException {
-        if (getTriggers() != null) {
-            getTriggers().beforeDelete(this);
-        }
-        {// delete cascade from peopleinterest
-            Peopleinterest[] records = (Peopleinterest[])Peopleinterest.load(getConnection(),"product_id = " + getProductId(),null);
-            for (int i = 0; i<records.length; i++) {
-                Peopleinterest peopleinterest = records[i];
-                peopleinterest.delete();
-            }
-        }
-        {// delete cascade from peopleproduct
-            Peopleproduct[] records = (Peopleproduct[])Peopleproduct.load(getConnection(),"product_id = " + getProductId(),null);
-            for (int i = 0; i<records.length; i++) {
-                Peopleproduct peopleproduct = records[i];
-                peopleproduct.delete();
-            }
-        }
         PreparedStatement ps = null;
         String stmt =
-                "DELETE FROM product " +
-                "WHERE product_id = " + getProductId();
+                "DELETE FROM dbversion " +
+                "WHERE dbversion_id = " + getDbversionId();
         try {
             ps = getConnection().prepareStatement(stmt);
             ps.execute();
         } finally {
             if (ps != null) ps.close();
         }
-        setProductId(new Integer(-getProductId().intValue()));
+        setDbversionId(new Integer(-getDbversionId().intValue()));
         if (getTriggers() != null) {
             getTriggers().afterDelete(this);
         }
     }
 
     public boolean isDeleted() {
-        return (getProductId().intValue() < 0);
+        return (getDbversionId().intValue() < 0);
     }
 
     public static DbObject[] load(Connection con,String whereCondition,String orderCondition) throws SQLException {
         ArrayList lst = new ArrayList();
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String stmt = "SELECT product_id,descr FROM product " +
+        String stmt = "SELECT dbversion_id,version_id,version FROM dbversion " +
                 ((whereCondition != null && whereCondition.length() > 0) ?
                 " WHERE " + whereCondition : "") +
                 ((orderCondition != null && orderCondition.length() > 0) ?
@@ -171,7 +159,7 @@ public class Product extends DbObject  {
             rs = ps.executeQuery();
             while (rs.next()) {
                 DbObject dbObj;
-                lst.add(dbObj=new Product(con,new Integer(rs.getInt(1)),rs.getString(2)));
+                lst.add(dbObj=new Dbversion(con,new Integer(rs.getInt(1)),new Integer(rs.getInt(2)),rs.getString(3)));
                 dbObj.setNew(false);
             }
         } finally {
@@ -181,10 +169,10 @@ public class Product extends DbObject  {
                 if (ps != null) ps.close();
             }
         }
-        Product[] objects = new Product[lst.size()];
+        Dbversion[] objects = new Dbversion[lst.size()];
         for (int i = 0; i < lst.size(); i++) {
-            Product product = (Product) lst.get(i);
-            objects[i] = product;
+            Dbversion dbversion = (Dbversion) lst.get(i);
+            objects[i] = dbversion;
         }
         return objects;
     }
@@ -196,7 +184,7 @@ public class Product extends DbObject  {
         boolean ok = false;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String stmt = "SELECT product_id FROM product " +
+        String stmt = "SELECT dbversion_id FROM dbversion " +
                 ((whereCondition != null && whereCondition.length() > 0) ?
                 "WHERE " + whereCondition : "");
         try {
@@ -214,41 +202,51 @@ public class Product extends DbObject  {
     }
 
     //public String toString() {
-    //    return getProductId() + getDelimiter();
+    //    return getDbversionId() + getDelimiter();
     //}
 
     public Integer getPK_ID() {
-        return productId;
+        return dbversionId;
     }
 
     public void setPK_ID(Integer id) throws ForeignKeyViolationException {
         boolean prevIsNew = isNew();
-        setProductId(id);
+        setDbversionId(id);
         setNew(prevIsNew);
     }
 
-    public Integer getProductId() {
-        return productId;
+    public Integer getDbversionId() {
+        return dbversionId;
     }
 
-    public void setProductId(Integer productId) throws ForeignKeyViolationException {
-        setWasChanged(this.productId != null && this.productId != productId);
-        this.productId = productId;
-        setNew(productId.intValue() == 0);
+    public void setDbversionId(Integer dbversionId) throws ForeignKeyViolationException {
+        setWasChanged(this.dbversionId != null && this.dbversionId != dbversionId);
+        this.dbversionId = dbversionId;
+        setNew(dbversionId.intValue() == 0);
     }
 
-    public String getDescr() {
-        return descr;
+    public Integer getVersionId() {
+        return versionId;
     }
 
-    public void setDescr(String descr) throws SQLException, ForeignKeyViolationException {
-        setWasChanged(this.descr != null && !this.descr.equals(descr));
-        this.descr = descr;
+    public void setVersionId(Integer versionId) throws SQLException, ForeignKeyViolationException {
+        setWasChanged(this.versionId != null && !this.versionId.equals(versionId));
+        this.versionId = versionId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) throws SQLException, ForeignKeyViolationException {
+        setWasChanged(this.version != null && !this.version.equals(version));
+        this.version = version;
     }
     public Object[] getAsRow() {
-        Object[] columnValues = new Object[2];
-        columnValues[0] = getProductId();
-        columnValues[1] = getDescr();
+        Object[] columnValues = new Object[3];
+        columnValues[0] = getDbversionId();
+        columnValues[1] = getVersionId();
+        columnValues[2] = getVersion();
         return columnValues;
     }
 
@@ -265,10 +263,15 @@ public class Product extends DbObject  {
     public void fillFromString(String row) throws ForeignKeyViolationException, SQLException {
         String[] flds = splitStr(row, delimiter);
         try {
-            setProductId(Integer.parseInt(flds[0]));
+            setDbversionId(Integer.parseInt(flds[0]));
         } catch(NumberFormatException ne) {
-            setProductId(null);
+            setDbversionId(null);
         }
-        setDescr(flds[1]);
+        try {
+            setVersionId(Integer.parseInt(flds[1]));
+        } catch(NumberFormatException ne) {
+            setVersionId(null);
+        }
+        setVersion(flds[2]);
     }
 }
