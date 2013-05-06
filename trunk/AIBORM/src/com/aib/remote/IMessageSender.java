058d1fc486bbb9fe2c0dbae2783db5573560e145
mukhin.nick <mukhin.nick@b3c5fbc4-fa22-48f7-9f9e-e458a875d5c6>
2013-05-06 18:25:49 +0000

    the server complete

---
+++ trunk/AIBORM/src/com/aib/remote/IMessageSender.java
@@ -0,0 +1,26 @@
+package com.aib.remote;
+
+//import com.xlend.orm.dbobject.DbObject;
+import com.aib.orm.dbobject.DbObject;
+import java.util.Vector;
+
+/**
+ *
+ * @author Nick Mukhin
+ */
+public interface IMessageSender extends java.rmi.Remote {
+    public DbObject[] getDbObjects(Class dbobClass, String whereCondition, 
+            String orderCondition) throws java.rmi.RemoteException;
+    public DbObject saveDbObject(DbObject dbob) throws java.rmi.RemoteException;
+    public void deleteObject(DbObject dbob) throws java.rmi.RemoteException;
+    public DbObject loadDbObjectOnID(Class dbobClass, int id) 
+            throws java.rmi.RemoteException;
+    public Vector[] getTableBody(String select) throws java.rmi.RemoteException;
+    public Vector[] getTableBody(String select, int page, int pagesize) throws java.rmi.RemoteException;
+    public int getCount(String select) throws java.rmi.RemoteException;
+    public boolean truncateTable(String tableName) throws java.rmi.RemoteException;
+    public void startTransaction(String transactionName) throws java.rmi.RemoteException;
+    public void commitTransaction() throws java.rmi.RemoteException;
+    public void rollbackTransaction(String transactionName) throws java.rmi.RemoteException;
+    public String getServerVersion() throws java.rmi.RemoteException;
+}
