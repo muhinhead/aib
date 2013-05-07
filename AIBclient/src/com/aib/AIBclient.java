78ef42a3f6d5634001b6015f80e43eb10e92cfa0
mukhin.nick <mukhin.nick@b3c5fbc4-fa22-48f7-9f9e-e458a875d5c6>
2013-05-07 17:45:20 +0000

    AIB client created

---
+++ AIBclient/src/com/aib/AIBclient.java
@@ -0,0 +1,247 @@
+package com.aib;
+
+import com.aib.orm.User;
+import com.aib.orm.dbobject.DbObject;
+import com.aib.remote.IMessageSender;
+import com.jidesoft.plaf.LookAndFeelFactory;
+import java.awt.Image;
+import java.awt.Window;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.prefs.Preferences;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+
+/**
+ *
+ * @author Nick Mukhin
+ */
+public class AIBclient {
+
+    public static final String version = "0.74.a";
+//    private static Userprofile currentUser;
+    private static Logger logger = null;
+    private static FileHandler fh;
+    private static Properties props;
+    private static final String PROPERTYFILENAME = "AIBclient.config";
+    private static User currentUser;
+    private static IMessageSender exchanger;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LookAndFeelFactory.installDefaultLookAndFeelAndExtension();
+        String serverIP = readProperty("ServerAddress", "localhost");
+        IMessageSender exchanger;
+        while (true) {
+            try {
+                exchanger = (IMessageSender) Naming.lookup("rmi://" + serverIP + "/AIBserver");
+                if (login(exchanger)) {
+                    new DashBoard("AIBclient",exchanger);
+                    break;
+                } else {
+                    System.exit(1);
+                }
+            } catch (Exception ex) {
+                logAndShowMessage(ex);
+                if ((serverIP = serverSetup("Check server settings")) == null) {
+                    System.exit(1);
+                } else {
+                    saveProps();
+                }
+            }
+        }
+    }
+
+    public static void logAndShowMessage(Throwable ne) {
+        JOptionPane.showMessageDialog(null, ne.getMessage(), "Error:", JOptionPane.ERROR_MESSAGE);
+        log(ne);
+    }
+
+    public static boolean login(IMessageSender exchanger) {
+        try {
+            new LoginImagedDialog(exchanger);//new Object[]{loginField, pwdField, exchanger});
+            return LoginImagedDialog.isOkPressed();
+        } catch (Throwable ee) {
+            JOptionPane.showMessageDialog(null, "Server failure\nCheck your logs please", "Error:", JOptionPane.ERROR_MESSAGE);
+            log(ee);
+        }
+        return false;
+    }
+
+    public static void log(String msg) {
+        log(msg, null);
+    }
+
+    public static void log(Throwable th) {
+        log(null, th);
+    }
+
+    private static void log(String msg, Throwable th) {
+        if (logger == null) {
+            try {
+                logger = Logger.getLogger("AIBclient");
+                fh = new FileHandler("%h/AIBclient.log", 1048576, 10, true);
+                logger.addHandler(fh);
+                logger.setLevel(Level.ALL);
+                SimpleFormatter formatter = new SimpleFormatter();
+                fh.setFormatter(formatter);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        logger.log(Level.SEVERE, msg, th);
+    }
+
+    public static void setWindowIcon(Window w, String iconName) {
+        w.setIconImage(loadImage(iconName, w));
+    }
+
+    public static Image loadImage(String iconName, Window w) {
+        Image im = null;
+        File f = new File("images/" + iconName);
+        if (f.exists()) {
+            try {
+                ImageIcon ic = new javax.swing.ImageIcon("images/" + iconName, "");
+                im = ic.getImage();
+            } catch (Exception ex) {
+                log(ex);
+            }
+        } else {
+            try {
+                im = ImageIO.read(w.getClass().getResourceAsStream("/" + iconName));
+            } catch (Exception ie) {
+                log(ie);
+            }
+        }
+        return im;
+    }
+
+    public static String readProperty(String key, String deflt) {
+        if (null == props) {
+            props = new Properties();
+            try {
+                File propFile = new File(PROPERTYFILENAME);
+                if (!propFile.exists() || propFile.length() == 0) {
+                    String curPath = propFile.getAbsolutePath();
+                    curPath = curPath.substring(0,
+                            curPath.indexOf(PROPERTYFILENAME)).replace('\\', '/');
+                    props.setProperty("user", "admin");
+                    props.setProperty("userPassword", "admin");
+                    propFile.createNewFile();
+                } else {
+                    props.load(new FileInputStream(propFile));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return deflt;
+            }
+        }
+        return props.getProperty(key, deflt);
+    }
+
+    public static void saveProps() {
+        if (props != null) {
+            if (getCurrentUser() != null) {
+                props.setProperty("LastLogin", getCurrentUser().getLogin());
+            }
+            props.setProperty("ServerAddress", props.getProperty("ServerAddress", "localhost:1099"));
+        }
+        Preferences userPref = Preferences.userRoot();
+        saveProperties();
+    }
+
+    public static void saveProperties() {
+        try {
+            if (props != null) {
+                props.store(new FileOutputStream(PROPERTYFILENAME),
+                        "-----------------------");
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            logAndShowMessage(e);
+        }
+    }
+
+    public static String serverSetup(String title) {
+        String address = readProperty("ServerAddress", "localhost");
+        String[] vals = address.split(":");
+        JTextField imageDirField = new JTextField(getProperties().getProperty("imagedir"));
+        JTextField addressField = new JTextField(16);
+        addressField.setText(vals[0]);
+        JSpinner portSpinner = new JSpinner(new SpinnerNumberModel(
+                vals.length > 1 ? new Integer(vals[1]) : 1099, 0, 65536, 1));
+        JComponent[] edits = new JComponent[]{imageDirField, addressField, portSpinner};
+        new ConfigEditor(title, edits);
+        if (addressField.getText().trim().length() > 0) {
+            String addr = addressField.getText() + ":" + portSpinner.getValue();
+            getProperties().setProperty("ServerAddress", addr);
+            getProperties().setProperty("imagedir", imageDirField.getText());
+            return addr;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return the currentUser
+     */
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    /**
+     * @param aCurrentUser the currentUser to set
+     */
+    public static void setCurrentUser(User aCurrentUser) {
+        currentUser = aCurrentUser;
+    }
+
+    public static Properties getProperties() {
+        return props;
+    }
+
+    public static List loadAllLogins(IMessageSender exchanger) {
+        try {
+            DbObject[] users = exchanger.getDbObjects(User.class, null, "login");
+            ArrayList logins = new ArrayList();
+            logins.add("");
+            int i = 1;
+            for (DbObject o : users) {
+                User up = (User) o;
+                logins.add(up.getLogin());
+            }
+            return logins;
+        } catch (RemoteException ex) {
+            log(ex);
+        }
+        return null;
+    }
+
+    public static IMessageSender getExchanger() {
+        return exchanger;
+    }
+
+    static void setExchanger(IMessageSender iMessageSender) {
+        exchanger = iMessageSender;
+    }
+}
---
+++ AIBclient/src/com/aib/ConfigEditor.java
@@ -0,0 +1,171 @@
+package com.aib;
+
+import com.aib.remote.IMessageSender;
+import com.xlend.util.PopupDialog;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.rmi.Naming;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Nick Mukhin
+ */
+public class ConfigEditor extends PopupDialog {
+
+    private JTextField imageDirField;
+    private JTextField addressField;
+    private JSpinner portSpinner;
+    private JButton testBtn;
+    private JButton okBtn;
+    private JButton cancelBtn;
+    private AbstractAction testAction;
+    private AbstractAction okAction;
+    private AbstractAction cancelAction;
+    private JButton chooseFldrBtn;
+    private AbstractAction chooseFldrAct;
+
+    public ConfigEditor(String title, Object obj) {
+        super(null, title, obj);
+    }
+
+    protected Color getHederBackground() {
+        return new Color(102, 125, 158);
+    }
+
+    protected void fillContent() {
+        super.fillContent();
+        String theme = AIBclient.readProperty("LookAndFeel",
+                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        try {
+            UIManager.setLookAndFeel(theme);
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception ex) {
+        }
+        
+
+        AIBclient.setWindowIcon(this, "aib.png");
+        JComponent[] comps = (JComponent[]) getObject();
+        imageDirField = (JTextField) comps[0];
+        addressField = (JTextField) comps[1];
+        portSpinner = (JSpinner) comps[2];
+
+        JPanel upperPanel = new JPanel(new BorderLayout(5, 5));
+        JPanel labelPanel = new JPanel(new GridLayout(3, 1, 5, 5));
+        JPanel editPanel = new JPanel(new GridLayout(3, 1, 5, 5));
+
+        labelPanel.add(new JLabel(" Default document's folder:", SwingConstants.RIGHT));
+        labelPanel.add(new JLabel("Server's IP or name:", SwingConstants.RIGHT));
+        labelPanel.add(new JLabel("Server's port:", SwingConstants.RIGHT));
+
+        JPanel imageFolderPanel = new JPanel(new BorderLayout(5, 5));
+        imageFolderPanel.add(imageDirField, BorderLayout.CENTER);
+        imageFolderPanel.add(chooseFldrBtn = new JButton(chooseFldrAct = new AbstractAction("...") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chooseFolder();
+            }
+        }), BorderLayout.EAST);
+        editPanel.add(imageFolderPanel);
+        editPanel.add(addressField);
+        JPanel portPanel = new JPanel(new BorderLayout());
+        portPanel.add(portSpinner, BorderLayout.WEST);
+        editPanel.add(portPanel);
+
+        upperPanel.add(editPanel, BorderLayout.CENTER);
+        upperPanel.add(labelPanel, BorderLayout.WEST);
+        upperPanel.add(new JPanel(), BorderLayout.EAST);
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(testBtn = new JButton(testAction = new AbstractAction("Test connection") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String serverIP = addressField.getText() + ":" + portSpinner.getValue();
+                try {
+                    IMessageSender exch = (IMessageSender) Naming.lookup("rmi://" + serverIP + "/AIBserver");
+                    JOptionPane.showMessageDialog(null, "Connection successful", "Ok!", JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Connection refused", "Error:", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }));
+        btnPanel.add(okBtn = new JButton(okAction = new AbstractAction("Ok") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        }) {
+        });
+        btnPanel.add(cancelBtn = new JButton(cancelAction = new AbstractAction("Cancel") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addressField.setText("");
+                portSpinner.setValue(new Integer(0));
+                dispose();
+            }
+        }));
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(upperPanel, BorderLayout.NORTH);
+        getContentPane().add(centerPanel, BorderLayout.CENTER);
+        getContentPane().add(btnPanel, BorderLayout.SOUTH);
+        getRootPane().setDefaultButton(okBtn);
+        //addressField.setMaximumSize(imageDirField.getPreferredSize());
+    }
+
+    private void chooseFolder() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+
+        chooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+
+            @Override
+            public String getDescription() {
+                return "*.*";
+            }
+        });
+
+        chooser.setDialogTitle("Choose folder to take attachment from");
+        chooser.setApproveButtonText("Choose");
+        int retVal = chooser.showOpenDialog(null);
+
+        if (retVal == JFileChooser.APPROVE_OPTION) {
+            imageDirField.setText(chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+
+    @Override
+    public void freeResources() {
+        testBtn.removeActionListener(testAction);
+        testAction = null;
+        okBtn.removeActionListener(okAction);
+        okAction = null;
+        cancelBtn.removeActionListener(cancelAction);
+        cancelAction = null;
+        chooseFldrBtn.removeActionListener(chooseFldrAct);
+        chooseFldrAct = null;
+    }
+}
