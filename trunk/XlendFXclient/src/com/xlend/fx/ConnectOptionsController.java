57595f82cbe46b2f4664904d91676dfa823ef371
mukhin.nick <mukhin.nick@b3c5fbc4-fa22-48f7-9f9e-e458a875d5c6>
2013-05-10 05:50:58 +0000

    the draft prototype of JavaFX client

---
+++ trunk/XlendFXclient/src/com/xlend/fx/ConnectOptionsController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xlend.fx;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+/**
+ * FXML Controller class
+ *
+ * @author nick
+ */
+public class ConnectOptionsController extends GeneralController implements Initializable, ControlledScreen {
+
+    private ScreensController myController;
+    @FXML
+    TextField pathField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+//        System.out.println("initialize of ConnectOptionsController");
+    }
+
+    @Override
+    public void setScreenParent(ScreensController screenParent) {
+        myController = screenParent;
+    }
+
+    @FXML
+    private void gotoOptionsScreen(ActionEvent event) {
+        myController.setScreen(XlendFXclient.screenLoginID);
+    }
+
+    @FXML
+    private void chooseDocPath(ActionEvent event) {
+
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Default document path");
+        File selectedDirectory = chooser.showDialog(null);
+        if (selectedDirectory != null) {
+            pathField.setText(selectedDirectory.getPath());
+        }
+    }
+}
