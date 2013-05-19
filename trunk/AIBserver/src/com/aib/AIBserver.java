95ec8947d51e9038da26f528e3bf1adfa6c32c4f
mukhin.nick <mukhin.nick@b3c5fbc4-fa22-48f7-9f9e-e458a875d5c6>
2013-05-19 16:42:15 +0000

    the people edit form improved

--- trunk/AIBserver/src/com/aib/AIBserver.java
+++ trunk/AIBserver/src/com/aib/AIBserver.java
@@ -37,7 +37,7 @@ import javax.swing.UIManager;
  */
 public class AIBserver {
 
-    private static final String version = "0.1";
+    private static final String version = "0.2";
     public static final String PROPERTYFILENAME = "AIBserver.config";
     private static final String ICONNAME = "aib.png";
     private static Logger logger = null;
