1ab0dba35a60587a47bdc5fee377f3c500951ab5
Nick Mukhin <mukhin.nick@gmail.com>
2016-02-07 10:31:25 +0100

    main GUI development

--- AIBserver/src/com/aib/AIBserver.java
+++ AIBserver/src/com/aib/AIBserver.java
@@ -37,7 +37,7 @@ import javax.swing.UIManager;
  */
 public class AIBserver {
 
-    private static final String version = "0.16";
+    private static final String version = "0.17";
     public static final String PROPERTYFILENAME = "AIBserver.config";
     private static final String ICONNAME = "aib.png";
     private static Logger logger = null;
