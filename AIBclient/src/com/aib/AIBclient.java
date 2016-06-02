8ffdae688384be967f00d6357694fc6abbca08de
Nick Mukhin <mukhin.nick@gmail.com>
2016-06-02 11:45:51 +0200

    bug fix

--- AIBclient/src/com/aib/AIBclient.java
+++ AIBclient/src/com/aib/AIBclient.java
@@ -60,7 +60,7 @@ import javax.swing.SpinnerNumberModel;
  */
 public class AIBclient {
 
-    private static final String version = "0.17.H";
+    private static final String version = "0.17.I";
 //    private static Userprofile currentUser;
     private static Logger logger = null;
     private static FileHandler fh;
